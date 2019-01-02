4211d05669ff422f9c68d1752ebd1222978d034b
Elmvx <elvmx47@aliyun.com>
2019-01-02 16:02:51 +0800

    轮播图

--- src/views/HomeFilms.vue
+++ src/views/HomeFilms.vue
@@ -1,15 +1,54 @@
 <template>
   <div class="home-films">
-    电影
+    <van-swipe :autoplay="3000">
+      <van-swipe-item v-for="item in banners" :key="item.bannerId">
+        <img :src="item.imgUrl" alt="" />
+      </van-swipe-item>
+    </van-swipe>
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import { Swipe, SwipeItem } from 'vant';
+Vue.use(Swipe).use(SwipeItem);
+
 export default {
-  name: 'HomeFilms'
+  name: 'HomeFilms',
+
+  data () {
+    return {
+      banners: [], // 轮播图图片
+    }
+  },
+
+  methods: {
+    /**
+     * 获取banner轮播图
+     */
+    getBanner () {
+      this.$http.get('/api/banner')
+        .then(res => {
+          if (res.code === 0) {
+            this.banners = res.data;
+          }
+        })
+    }
+  },
+
+  created () {
+    this.getBanner();
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.home-films {
+  .van-swipe {
+    height: 210px;
+    img {
+      width: 100%;
+    }
+  }
+}
 </style>
