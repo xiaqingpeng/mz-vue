a9ff1b4c188eeceb63c2005fde5e3ac5f710ec19
Elmvx <elvmx47@aliyun.com>
2019-01-09 16:44:12 +0800

    我的页面

--- src/views/Home/Center/Index.vue
+++ src/views/Home/Center/Index.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="home-center">
-    我的
+    <div class="avatar">
+      <img src="" class="avator-icon" />
+      <div class="nick-name">立即登录</div>
+    </div>
+
+    <div class="margin-set my-card">
+      <img src="./icon/card.png" alt="" class="icon" />
+      <span class="label">卖座卡</span>
+      <img src="./icon/arrow.png" alt="" class="arrow" />
+    </div>
+    <div class="margin-set my-balance">
+      <img src="./icon/balance.png" alt="" class="icon" />
+      <span class="label">余额</span>
+      <img src="./icon/arrow.png" alt="" class="arrow">
+    </div>
+    <div class="margin-set system-set">
+      <img src="./icon/system-set.png" alt="" class="icon" />
+      <span class="label">设置</span>
+      <img src="./icon/arrow.png" alt="" class="arrow">
+    </div>
   </div>
 </template>
 
@@ -10,6 +29,54 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '@/styles/common/mixins.scss';
 
+.home-center {
+  .avatar {
+    text-align: center;
+    margin-top: -44px;
+    height: 200px;
+    padding-left: 22px;
+    padding-top: 64px;
+    background: url(https://assets.maizuo.com/h5/v5/public/app/img/bg.6837f67.png);
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    color: #fff;
+    margin-bottom: 10px;
+    .avator-icon {
+      width: 63px;
+      height: 63px;
+      border-radius: 35px;
+      margin-right: 20px;
+      border: 2px solid #fff;
+    }
+    .nick-name {
+      font-size: 16px;
+    }
+  }
+  .margin-set {
+    @include border-bottom-1px;
+    background: #fff;
+    padding: 0 15px;
+    height: 49px;
+    display: flex;
+    align-items: center;
+    .label {
+      margin-left: 15px;
+      color: #191a1b;
+      flex: 1;
+    }
+    .icon {
+      width: 20px;
+      height: 20px;
+    }
+    .arrow {
+      text-align: right;
+      width: 6px;
+      height: 10px;
+    }
+  }
+}
 </style>
